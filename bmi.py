6f367792d6a2c0c274b27afd314debd2b1ec1b87
PorYu <yu07.24@ichat.sp.edu.sg>
2025-10-25 20:42:16 +0800

    new bmi py

---
+++ bmi.py
@@ -0,0 +1,15 @@
+def calculate_bmi(height, weight):
+    print("Height = " + str(height))
+    print("Weight = " + str(weight))
+    bmi=weight/(height**2)
+    if bmi>25.0:
+        print(bmi)
+        print("overweight")
+    elif bmi>=18.5:
+        print(bmi)
+        print("Normal weight")
+    else: 
+        print(bmi)
+        print("underweight")
+
+calculate_bmi(weight=73, height=20)
